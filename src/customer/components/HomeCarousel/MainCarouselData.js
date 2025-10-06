b4470e731fe992b413927f45aa4fb65da62e0011
MD-Muddabir <md.danish0086@gmail.com>
2025-10-07 01:44:49 +0530

    I can add the home page

---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,18 @@
+export const mainCarouselData = [
+    {
+        image: "https://welpix.b-cdn.net/wp-content/uploads/2024/03/Enhancing-your-skills-in-dynamic-electronic-product-photography.webp",
+        path: "/women/clothing/lengha_choli"
+    },
+    {
+        image: "https://welpix.b-cdn.net/wp-content/uploads/2024/03/The-art-of-storytelling-in-electronic-product-photography.webp",
+        path: "/women/clothing/women_dress"
+    },
+    {
+        image: "https://cdn.open-pr.com/T/6/T616614381_g.jpg",
+        path: "/women/clothing/women_dress"
+    },
+    {
+        image: "https://www.schott.com/-/media/project/onex/shared/teasers/consumer-electronics/consumer-electronics_01-displays_00_720x450.jpg?rev=fe6df7a508644a7d8e294e6c294a317b",
+        path: "/women/clothing/women_saree"
+    },
+]
